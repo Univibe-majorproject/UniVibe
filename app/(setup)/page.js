66786114f8e2edd192dda480aefa4f650a8f88a9
Prakarti Goel <prakartigoel24@gmail.com>
2023-12-22 23:07:14 +0530

    Formatting fix

--- app/(setup)/page.js
+++ app/(setup)/page.js
@@ -20,7 +20,7 @@ const SetupPage = async () => {
     return redirect(`/servers/${server.id}`);
   }
 
-  return <InititalModal/>;
+  return <InititalModal />;
 };
 
 export default SetupPage;
--- components/modals/initial-modal.jsx
+++ components/modals/initial-modal.jsx
@@ -1,130 +1,120 @@
 "use client";
 import * as z from "zod";
-import { zodResolver } from "@hookform/resolvers/zod"
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { useEffect, useState } from "react";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-} from "@/components/ui/dialog"
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
-import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "@/components/ui/form"
-
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
-
-
-//creating our form schema 
+//creating our form schema
 const formSchema = z.object({
-    name: z.string().min(1, {
-        message: "Server name is required."
-    }),
-    imageUrl: z.string().min(1, {
-        message: "Server image is required."
-    })
-})
+  name: z.string().min(1, {
+    message: "Server name is required.",
+  }),
+  imageUrl: z.string().min(1, {
+    message: "Server image is required.",
+  }),
+});
 
-export const InititalModal = ()=> {
-    const [isMounted, setIsMounted] = useState(false);
+export const InititalModal = () => {
+  const [isMounted, setIsMounted] = useState(false);
 
-    useEffect(() => {
-      setIsMounted(true);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
-    }, []);
-    
-    const form = useForm({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            name: "",
-            imageUrl: "",
-        }
-    });
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      imageUrl: "",
+    },
+  });
 
-    const isLoading = form.formState.isSubmitting;
+  const isLoading = form.formState.isSubmitting;
 
-    //on submit function that only logs our values for now 
-    const onSubmit = async (values) => {
-        console.log(values);
-    }
+  //on submit function that only logs our values for now
+  const onSubmit = async (values) => {
+    console.log(values);
+  };
 
-    if(!isMounted) {
-        return null;
-    }
-    return (
-        <Dialog open>
-            <DialogContent className="bg-white text-black p-0 overflow-hidden"> 
-                <DialogHeader className="pt-8 px-6">
-                    <DialogTitle className="text-2xl text-center font-bold">
-                        Customize your server
-                    </DialogTitle>
-                    <DialogDescription className="text-center text-zinc-500"> 
-                        Give your server a personality with a name and an image. 
-                        You can always change it later.
-                    </DialogDescription>
-                </DialogHeader>
+  if (!isMounted) {
+    return null;
+  }
+  return (
+    <Dialog open>
+      <DialogContent className="bg-white text-black p-0 overflow-hidden">
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Customize your server
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500">
+            Give your server a personality with a name and an image. You can
+            always change it later.
+          </DialogDescription>
+        </DialogHeader>
 
-                <Form {...form}>  
-                    <form onSubmit={form.handleSubmit(onSubmit)} 
-                    className="space-y-8">
-                      <div className="space-y-8 px-6">
-                        <div className="flex items-center justify-center text-center">
-                            TODO: Image Upload
-                        </div>
-                        
-                       <FormField
-                        control={form.control}
-                        name="name"
-                        render={({field}) => (
-                            <FormItem>
-                                <FormLabel
-                                className="uppercase text-xs font-bold text-zinc-500
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <div className="space-y-8 px-6">
+              <div className="flex items-center justify-center text-center">
+                TODO: Image Upload
+              </div>
+
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel
+                      className="uppercase text-xs font-bold text-zinc-500
                                 dark:text-secondary/70"
-                                >
-                                    Server Name
-                            </FormLabel>
+                    >
+                      Server Name
+                    </FormLabel>
 
-                            <FormControl>
-                                <Input
-                                    disabled={`${isLoading}`}
-                                    className="bg-zinc-300/50 border-0
+                    <FormControl>
+                      <Input
+                        disabled={`${isLoading}`}
+                        className="bg-zinc-300/50 border-0
                                     focus-visible:ring-0 text-black 
                                     focus-visible:ring-offset-0"
-                                    placeholder="Enter server name"
-                                    {...field}
-                                />
-                            </FormControl>
-                            <FormMessage/>
-
-                            </FormItem>
-                        )}
-                       />
-                    
-
-                      </div> 
-                    <DialogFooter className={"bg-grey-100 px-6 py-4"}>
-                        <Button disable={`${isLoading}`}
-                        variant="primary">
-                            Create
-                        </Button>
-                    </DialogFooter> 
-                    </form>
-                </Form>
-
-            </DialogContent> 
-        </Dialog>
-    )
-}
+                        placeholder="Enter server name"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className={"bg-grey-100 px-6 py-4"}>
+              <Button disable={`${isLoading}`} variant="primary">
+                Create
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
