d0477f0edfe70c65703c01520879c50db3b364b6
Sandeep Lal <sandeep221102@gmail.com>
2023-12-24 02:13:24 +0530

    added navigation sidebar

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,3 +1,5 @@
+import { NavigationSidebar } from "@/components/navigation/navigation-sidebar";
+
 const MainLayout = async ({
     children
 }) => {
@@ -5,7 +7,7 @@ const MainLayout = async ({
         <div className="h=full">
             <div className="hidden md:flex h-full w-[72px]
             z-30 flex-col fixed inset-y-0">   
-                
+                <NavigationSidebar/>
             </div>
             <main className="md:pl-[72px] h-full">
                 {children}
---
+++ components/navigation/navigation-sidebar.jsx
@@ -0,0 +1,7 @@
+export const NavigationSidebar = () => {
+    return (
+        <div>
+            Navigation Sidebar
+        </div>
+    )
+}
