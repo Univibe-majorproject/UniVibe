6b9d318b23eda265138e6fc3a39ece14764723ce
Sandeep Lal <sandeep221102@gmail.com>
2023-12-24 02:32:11 +0530

    updated navigation sidebar, added navigation-action button and tooltip and seperator component from shadcn

--- components/navigation/navigation-sidebar.jsx
+++ components/navigation/navigation-sidebar.jsx
@@ -1,7 +1,34 @@
-export const NavigationSidebar = () => {
+import { currentProfile } from "@/lib/current-profile"
+
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+
+import NavigationAction from "./navigation-action";
+
+export const NavigationSidebar = async () => {
+
+    const profile = await currentProfile();
+
+    if(!profile){
+        return redirect("/");
+    }
+
+    //to find all the server this user is a part of 
+    const servers = await db.server.findMany({
+        where: {
+            members: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        }
+    });
+
     return (
-        <div>
-            Navigation Sidebar
+        <div className="space-y-4 flex flex-col items-center
+        h-full text-primary w-full dark:bg-[#1E1F22]
+        py-3">
+            <NavigationAction/>
         </div>
     )
 }
