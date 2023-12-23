b246f54a45491b4b654bab1b0518e94d59bbb630
Sandeep Lal <sandeep221102@gmail.com>
2023-12-23 19:06:13 +0530

    add: server image upload diplayed

--- components/file-upload.jsx
+++ components/file-upload.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { X } from "lucide-react";
+import Image from "next/image";
 import { UploadDropzone } from "@/lib/uploadthing";
 import "@uploadthing/react/styles.css";
 
@@ -8,6 +10,22 @@ export const FileUpload = ({
     value,
     endpoint
 }) => {
+    // check if we have a value? render something : not 
+    const fileType = value?.split(".").pop();
+    if(value && fileType !== "pdf") {
+        return (
+            <div className="relative h-20 w-20">
+                <Image
+                    fill
+                    src={value}
+                    alt="Upload"
+                    className="rounded-full"
+                />
+            </div>
+        )
+    }
+
+
     return(
         <UploadDropzone
          endpoint={endpoint}
