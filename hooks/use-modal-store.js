c7a2ac028255d35f400fc6bc2870604ed7e2a4ea
Sandeep Lal <sandeep221102@gmail.com>
2023-12-24 23:30:56 +0530

    add: created use-modal-store.js

---
+++ hooks/use-modal-store.js
@@ -0,0 +1,12 @@
+//hook to control all modal in our application 
+import { create } from "zustand";
+
+export const ModalType = "createServer";
+
+
+export const userModal = create((set) => ({
+   type: null,
+   isOpen: false,
+   onOpen: (type) => set({ isOpen: true, type}),
+   onClose: ()=> set({type:null, isOpen: false}) 
+}));
