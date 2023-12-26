5ddfbebd8c89551eab5136719e480bc723d9e723
Sandeep Lal <sandeep221102@gmail.com>
2023-12-27 02:27:34 +0530

    Added drop down menu trigger icons

--- components/modals/members-modal.jsx
+++ components/modals/members-modal.jsx
@@ -1,6 +1,5 @@
 "use client";
 
-
 import {
   Dialog,
   DialogContent,
@@ -9,20 +8,41 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 
+import { 
+  ShieldCheck, 
+  ShieldAlert,
+  MoreVertical,
+  ShieldQuestion,
+} from "lucide-react";
 import { useModal } from "@/hooks/use-modal-store";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { UserAvatar } from "@/components/user-avatar";
+import { useState } from "react";
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
+const roleIconMap = {
+  GUEST: null,
+  MODERATOR: <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500" />,
+  ADMIN: <ShieldAlert className="h-4 w-4 text-rose-500" />,
+};
 
 export const MembersModal = () => {
-
-  const { onOpen, isOpen, onClose, type, data } = useModal(); 
-
+  const { onOpen, isOpen, onClose, type, data } = useModal();
+  const [loadingId, setLoadingId] = useState("");
 
   const isModalOpen = isOpen && type === "members";
   const { server } = data;
-
 
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
@@ -31,28 +51,56 @@ export const MembersModal = () => {
           <DialogTitle className="text-2xl text-center font-bold">
             Manage Members
           </DialogTitle>
-          <DialogDescription
-          className="text-center text-zinc-500"
-        >
-          {server?.members?.length} Members
-        </DialogDescription>
-        
+          <DialogDescription className="text-center text-zinc-500">
+            {server?.members?.length} Members
+          </DialogDescription>
         </DialogHeader>
-        
-        <ScrollArea className="mt-8 max-h-[420px] pr-6">
-            {server?.members?.map((member)=>(
-              <div key={member.id} className="flex items-center
-              gap-x-2 mb-6">
-                <UserAvatar src={member.profile.imageUrl} />
-                <div className="flex flex-col gap-y-1">
-                  <div className="text-xs font-semibold flex items-center">
-                    {member.profile.name}
-                  </div> 
-                </div>
-              </div>
-            ))}
-        </ScrollArea>
 
+        <ScrollArea className="mt-8 max-h-[420px] pr-6">
+          {server?.members?.map((member) => (
+            <div
+              key={member.id}
+              className="flex items-center
+              gap-x-2 mb-6"
+            >
+              <UserAvatar src={member.profile.imageUrl} />
+              <div className="flex flex-col gap-y-1">
+                <div
+                  className="text-xs font-semibold 
+                  flex items-center gap-x-1"
+                >
+                  {member.profile.name}
+                  {roleIconMap[member.role]}
+                </div>
+
+                <p className="text-xs text-zinc-500">
+                  {member.profile.email}
+                </p>
+              </div>
+              {server.profileId !== member.profileId &&
+              loadingId !== member.id && (
+                <div className="ml-auto">
+                  <DropdownMenu>
+                    <DropdownMenuTrigger>
+                      <MoreVertical className="h-4 w-4 text-zinc-500" />
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent side="left">
+                      <DropdownMenuSub>
+                        <DropdownMenuSubTrigger
+                          className="flex items-center"
+                        >
+                          <ShieldQuestion 
+                            className="w-4 h-4 mr-2" />
+                            <span>Role</span>
+                        </DropdownMenuSubTrigger>
+                      </DropdownMenuSub>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </div>
+              )}
+            </div>
+          ))}
+        </ScrollArea>
       </DialogContent>
     </Dialog>
   );
