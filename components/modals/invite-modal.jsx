3a60d1082c74da7891f3c7aab3d455960680b65c
Sandeep Lal <sandeep221102@gmail.com>
2023-12-26 20:22:11 +0530

    modified invite-modal.jsx

--- components/modals/invite-modal.jsx
+++ components/modals/invite-modal.jsx
@@ -2,12 +2,16 @@
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
 
+import { Copy, RefreshCw }  from "lucide-react"
+
 import { useModal } from "@/hooks/use-modal-store";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export const InviteModal = () => {
   const { isOpen, onClose, type } = useModal();  
@@ -20,16 +24,39 @@ export const InviteModal = () => {
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
-            Customize your server
+            Invite People 
           </DialogTitle>
-          <DialogDescription className="text-center text-zinc-500">
-            Give your server a personality with a name and an image. You can
-            always change it later.
-          </DialogDescription>
+        
         </DialogHeader>
+        <div className="p-6">
+            <Label className="uppercase text-xs font-bold
+             text-zinc-500 dark:text-secondary/70">
+              Server Invite Link
+            </Label>
+            <div className="flex items-center mt-2 gap-x-2">
+                <Input 
+                  className="bg-zinc-300/50 border-0 focus-visible:ring-0
+                  text-black focus-visible:ring-offset-0"
+                  value="invite-link"
+                />
 
-            Invite Modal! 
+                <Button size="icon">
+                  <Copy className="w-4 h-4"/>
+                </Button>
+            </div>
+            <Button
+              variant = "link"
+              size = "sm"
+              className = "text-xs text-zinc-500 mt-4"
+            >
+              Generate a new link
+              <RefreshCw 
+               className="w-4 h-4 ml-2" 
+              >
 
+              </RefreshCw>
+            </Button>
+        </div>
       </DialogContent>
     </Dialog>
   );
