d7998ed57297e89de40a06187484c1e27f80620d
Sandeep Lal <sandeep221102@gmail.com>
2023-12-28 01:22:35 +0530

    modified create-channel-modal

--- components/modals/create-channel-modal.jsx
+++ components/modals/create-channel-modal.jsx
@@ -7,7 +7,6 @@ import axios from "axios";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
@@ -24,7 +23,6 @@ import {
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { FileUpload } from "@/components/file-upload";
 import { useRouter } from "next/navigation";
 import { useModal } from "@/hooks/use-modal-store";
 
@@ -32,9 +30,6 @@ import { useModal } from "@/hooks/use-modal-store";
 const formSchema = z.object({
   name: z.string().min(1, {
     message: "Server name is required.",
-  }),
-  imageUrl: z.string().min(1, {
-    message: "Server image is required.",
   }),
 });
 
@@ -48,7 +43,6 @@ export const CreateChannelModal = () => {
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
-      imageUrl: "",
     },
   });
 
@@ -78,35 +72,13 @@ export const CreateChannelModal = () => {
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
-            Customize your server
+            Create Channel
           </DialogTitle>
-          <DialogDescription className="text-center text-zinc-500">
-            Give your server a personality with a name and an image. You can
-            always change it later.
-          </DialogDescription>
         </DialogHeader>
 
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-8 px-6">
-              <div className="flex items-center justify-center text-center">
-                <FormField
-                  control={form.control}
-                  name="imageUrl"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <FileUpload
-                          endpoint="serverImage"
-                          value={field.value}
-                          onChange={field.onChange}
-                        />
-                      </FormControl>
-                    </FormItem>
-                  )}
-                />
-              </div>
-
               <FormField
                 control={form.control}
                 name="name"
@@ -116,7 +88,7 @@ export const CreateChannelModal = () => {
                       className="uppercase text-xs font-bold text-zinc-500
                                 dark:text-secondary/70"
                     >
-                      Server Name
+                      Channel name
                     </FormLabel>
 
                     <FormControl>
@@ -125,7 +97,7 @@ export const CreateChannelModal = () => {
                         className="bg-zinc-300/50 border-0
                                     focus-visible:ring-0 text-black 
                                     focus-visible:ring-offset-0"
-                        placeholder="Enter server name"
+                        placeholder="Enter channel name"
                         {...field}
                       />
                     </FormControl>
