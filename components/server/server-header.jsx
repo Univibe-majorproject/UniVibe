02a0d4e5aa5652af5942b09754ade4825361b721
Sandeep Lal <sandeep221102@gmail.com>
2023-12-28 01:17:29 +0530

    updated: create-channel-modal.jsx, modal-provider.jsx and server-header.jsx

--- components/server/server-header.jsx
+++ components/server/server-header.jsx
@@ -71,7 +71,9 @@ export const ServerHeader = ({ server, role }) => {
           </DropdownMenuItem>
         )}
         {isModerator && (
-          <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={()=> onOpen("createChannel")} 
+            className="px-3 py-2 text-sm cursor-pointer">
             Create Channel
             <PlusCircle className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
