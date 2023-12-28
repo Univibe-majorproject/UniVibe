4cdc13f76790e4c2e5d9d8ffcc0b84d985c89e88
Sandeep Lal <sandeep221102@gmail.com>
2023-12-29 01:27:44 +0530

    add: text channel tag, click on plus icon to create a channel

--- components/server/server-section.jsx
+++ components/server/server-section.jsx
@@ -1,6 +1,8 @@
 "use client";
 import { ChannelType, MemberRole } from "@prisma/client";
-
+import { ActionTooltip } from "@/components/action-tooltip";
+import { Plus } from "lucide-react"; 
+import { useModal } from "@/hooks/use-modal-store";
 
 export const ServerSection = ({
     label, 
@@ -9,9 +11,27 @@ export const ServerSection = ({
     ChannelType,
     server,
 }) =>{
+
+    const { onOpen } = useModal();
+
     return (
-        <div>
-            Server Section
+        <div className="flex items-center justify-between py-2">
+            <p className="text-xs uppercase font-semibold text-zinc-500
+            dark:text-zinc-400">
+                {label}
+            </p>
+            {role !== MemberRole.GUEST && sectionType === "channels" && (
+                <ActionTooltip label="Create Channel" side="top">
+                    <button
+                    onClick={()=> onOpen("createChannel")}
+                        className="text-zinc-500 hover:text-zinc-600
+                        dark:text-zinc-400 dark:hover:text-zinc-300
+                        transition"
+                    >
+                        <Plus className="h-4 w-4"/>
+                    </button>
+                </ActionTooltip>
+            )}
         </div>
     )
 }
