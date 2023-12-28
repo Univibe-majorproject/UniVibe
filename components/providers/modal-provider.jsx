01df8069d8417a3612159d8feb9247a49e0c4677
Sandeep Lal <sandeep221102@gmail.com>
2023-12-28 15:26:23 +0530

    added cancel and confirm button on leave server dialog box

--- components/providers/modal-provider.jsx
+++ components/providers/modal-provider.jsx
@@ -5,6 +5,7 @@ import { InviteModal } from "@/components/modals/invite-modal";
 import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { MembersModal } from "@/components/modals/members-modal";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
+import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 
 export const ModalProvider = () => {
   //handling isMount here
@@ -26,6 +27,7 @@ export const ModalProvider = () => {
             <EditServerModal/>
             <MembersModal/>
             <CreateChannelModal/>
+            <LeaveServerModal/>
         </>
      );
 }
