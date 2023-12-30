7422901300f37662093dda8e54b6c69109e5aab3
Sandeep Lal <sandeep221102@gmail.com>
2023-12-30 21:01:46 +0530

    updated use-modal-store.js for deleteChannel

--- hooks/use-modal-store.js
+++ hooks/use-modal-store.js
@@ -2,7 +2,7 @@
 import { create } from "zustand";
 
 export const ModalType = "createServer" | "invite" | "editServer" | "members" 
-| "createChannel" | "leaveServer" | "deleteServer";
+| "createChannel" | "leaveServer" | "deleteServer" | "deleteChannel";
 
 export const useModal = create((set) => ({
   type: null,
