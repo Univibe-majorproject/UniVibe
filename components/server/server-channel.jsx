ed89bd8b418725e3fc718cbc55140133e5d57e71
Sandeep Lal <sandeep221102@gmail.com>
2023-12-31 21:58:15 +0530

    add: onClick and onAction function on channels for redirection to that page update: server-member.jsx memberId === member.id

--- components/server/server-channel.jsx
+++ components/server/server-channel.jsx
@@ -1,10 +1,7 @@
 "use client";
 
 import { useParams, useRouter } from "next/navigation";
-import {
-    ChannelType, 
-    MemberRole, 
-} from "@prisma/client";
+import { ChannelType, MemberRole } from "@prisma/client";
 
 import { Hash, Mic, Video, Edit, Trash, Lock } from "lucide-react";
 import { cn } from "@/lib/utils";
@@ -25,9 +22,18 @@ export const ServerChannel = ({ channel, server, role }) => {
   //to create dynamic icons
   const Icon = iconMap[channel.type];
 
+  const onClick = () => {
+    router.push(`/servers/${params?.serverId}/channels/${channel.id}`);
+  };
+
+  const onAction = (e, action) => {
+    e.stopPropagation();
+    onOpen(action, { channel, server });
+  };
+
   return (
     <button
-      onClick={() => {}}
+      onClick={onClick}
       className={cn(
         "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
         params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
@@ -48,13 +54,13 @@ export const ServerChannel = ({ channel, server, role }) => {
         <div className="ml-auto flex items-center gap-x-2">
           <ActionTooltip label="Edit">
             <Edit
-              onClick={() => onOpen("editChannel",{server, channel})}
+              onClick={(e) => onAction(e, "editChannel")}
               className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
             />
           </ActionTooltip>
           <ActionTooltip label="Delete">
             <Trash
-              onClick={() => onOpen("deleteChannel", { server, channel })}
+              onClick={(e) => onAction(e, "deleteChannel")}
               className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
             />
           </ActionTooltip>
