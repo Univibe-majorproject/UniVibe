69538be20cfb782d2be5268965eaaea17030eae3
Sandeep Lal <sandeep221102@gmail.com>
2023-12-31 23:10:15 +0530

    add: mobile-toggle.jsx and sheet component from shadcn  update: chat-header to dispaly name of the channel

--- components/chat/chat-header.jsx
+++ components/chat/chat-header.jsx
@@ -1,7 +1,5 @@
-import {
-    Hash,
-    Menu,
-} from "lucide-react";
+import { Hash } from "lucide-react";
+import { MobileToggle } from "@/components/mobile-toggle";
 
 export const ChatHeader = ({
     serverId,
@@ -11,11 +9,13 @@ export const ChatHeader = ({
 }) => {
     return (
         <div className="text-md font-semibold px-3 flex items-center h-12 border-x-neutral-200 dark:border-neutral-800 border-b-2">
-            <Menu/>
+            <MobileToggle/>
             {type === "channel" && (
                 <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2"/>
             )}
-            
+            <p className="font-semibold text-md text-black dark:text-white">
+                {name}
+            </p>
         </div>
     )
 }
---
+++ components/mobile-toggle.jsx
@@ -0,0 +1,7 @@
+import { Menu } from "lucide-react";
+
+export const MobileToggle = () => {
+    return (
+        <Menu />
+    )
+}
