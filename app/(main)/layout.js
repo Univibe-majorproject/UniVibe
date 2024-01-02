071870ceeae9bca4b019b1f13a498277968f0ba2
Prakarti Goel <prakartigoel24@gmail.com>
2024-01-02 22:24:20 +0530

    Init sidenav-items restructure

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -4,7 +4,7 @@ const MainLayout = async ({ children }) => {
   return (
     <div className="h=full">
       <div
-        className="hidden md:flex h-full w-[72px]
+        className="hidden md:flex md:flex-row h-[72px] w-full
             z-30 flex-col fixed inset-y-0"
       >
         <NavigationSidebar />
--- components/navigation/navigation-item.jsx
+++ components/navigation/navigation-item.jsx
@@ -16,13 +16,13 @@ export const NavigationItem = ({ id, imageUrl, name }) => {
   return (
     <ActionTooltip side="right" align="center" label={name}>
       <button onClick={onClick} className="group relative flex items-center">
-        <div
+        {/* <div
           className={cn(
             "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
             params?.serverId !== id && "group-hover:h-[20px]",
             params?.serverId === id ? "h-[36px]" : "h-[8px]"
           )}
-        />
+        /> */}
         <div
           className={cn(
             "relative group flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
