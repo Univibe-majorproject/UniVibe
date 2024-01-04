a8d511289a03d9d48b7ced1b7098548afd5ebeaa
Prakarti Goel <prakartigoel24@gmail.com>
2024-01-04 22:16:04 +0530

    Added message-file-modal to plus button in chat-input to upload attachment

--- components/chat/chat-input.jsx
+++ components/chat/chat-input.jsx
@@ -15,6 +15,7 @@ import {
   FormItem,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { useModal } from "@/hooks/use-modal-store";
 
 const formSchema = z.object({
   content: z.string().min(1),
@@ -27,6 +28,7 @@ export const ChatInput = ({
   type,
 }) => {
 
+  const {onOpen} = useModal();
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -60,7 +62,7 @@ export const ChatInput = ({
                 <div className="relative p-4 pb-6">
                   <button
                     type="button"
-                    onClick={() =>{}}
+                    onClick={() =>onOpen("messageFile", {apiUrl, query})}
                     className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
                   >
                     <Plus className="text-white dark:text-[#313338]" />
