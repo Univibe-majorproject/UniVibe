af3eaf355720d6618194492d3a91463eaa389602
Prakarti Goel <prakartigoel24@gmail.com>
2024-01-04 22:43:17 +0530

    Mod: file-upload.jsx to show preview of the uploaded pdf attachment

--- components/file-upload.jsx
+++ components/file-upload.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { X } from "lucide-react";
+import { FileIcon } from "lucide-react";
 import Image from "next/image";
 import { UploadDropzone } from "@/lib/uploadthing";
 import "@uploadthing/react/styles.css";
@@ -24,6 +25,28 @@ export const FileUpload = ({ onChange, value, endpoint }) => {
     );
   }
 
+  if(value && fileType === "pdf")
+  {
+    return (
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400"/>
+        <a 
+        href={value} 
+        target="_blank" 
+        rel="noopener noreferrer"
+        className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline">{value}
+        </a>
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1
+                    rounded-full absolute -top-2 -right-2 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    )
+  }
   return (
     <UploadDropzone
       endpoint={endpoint}
