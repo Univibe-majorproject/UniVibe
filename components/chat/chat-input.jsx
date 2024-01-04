28788b0dccc096954cd60c96273ea91be6704e68
Prakarti Goel <prakartigoel24@gmail.com>
2024-01-04 14:29:22 +0530

    Add: useForm hook, isLoading state, onSubmit(dummy) function

--- components/chat/chat-input.jsx
+++ components/chat/chat-input.jsx
@@ -1,12 +1,34 @@
 "use client";
 import * as z from "zod";
+import { useForm } from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
 
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input"
 
 const formSchema = z.object({
     content: z.string().min(1)
 })
 
 export const ChatInput = ({apiUrl, query, name, type})=>{
+    const form = useForm({
+        resolver:zodResolver(formSchema),
+        defaultValues:{
+            content:""
+        }
+    });
+
+    const isLoading = form.formState.isSubmitting;
+    const onSubmit = async(values)=>{
+        console.log(values);
+    }
+    
+
     return (
         <div>
             Chat Input
