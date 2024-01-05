7258a436ac21779707397f4c79d4c8613c8118f8
Prakarti Goel <prakartigoel24@gmail.com>
2024-01-05 15:11:35 +0530

    Rendered the messages on screen

--- components/chat/chat-messages.jsx
+++ components/chat/chat-messages.jsx
@@ -2,7 +2,7 @@
 
 import { Fragment} from "react";
 import { Loader2, ServerCrash } from "lucide-react";
-
+import {Member, Message, Profile} from "@prisma/client";
 import { useChatQuery } from "@/hooks/use-chat-query";
 import { ChatWelcome } from "./chat-welcome";
 
@@ -56,13 +56,24 @@ export const ChatMessages = ({
   }
 
   return (
-        <div className="flex-1 flex flex-col py-4 overflow-y-auto">
+        <div className="flex-1 flex flex-col py-4 overflow-y-auto relative left-8">
             <div className="flex-1">
                 <ChatWelcome
                 type={type}
                 name={name} 
                 />
             </div>
+            <div className="flex flex-col-reverse mt-auto">
+                {data?.pages?.map((group, i) => (
+                <Fragment key={i}>
+                    {group.items.map((message) => (
+                    <div key={message.id}>
+                        {message.content}
+                    </div>
+                ))}
+                </Fragment>
+            ))}
+      </div>
         </div>
   )
 }
