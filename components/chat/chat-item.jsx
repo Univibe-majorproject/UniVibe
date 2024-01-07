eba38bb4a3833736d0680bb9e302b9302b881e15
Prakarti Goel <prakartigoel24@gmail.com>
2024-01-07 15:15:05 +0530

    Add: UserAvatar and user's name who sent the message

--- components/chat/chat-item.jsx
+++ components/chat/chat-item.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Member, MemberRole, Profile } from "@prisma/client";
+import { UserAvatar } from "@/components/user-avatar";
 
 export const ChatItem = ({
   id,
@@ -14,7 +15,22 @@ export const ChatItem = ({
   socketUrl,
   socketQuery
 }) => { 
-    <div>
-        chat item
+    return (
+        <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
+        <div className="group flex gap-x-2 items-start w-full">
+            <div className="cursor-pointer hover:drop-shadow-md transition">
+                <UserAvatar src={member.profile.imageUrl}/>
+            </div>
+            <div className="flex flex-col w-full">
+                <div className="flex items-center gap-x-2">
+                    <div className="flex items-center">
+                        <p className="font-semibold text-sm hover:underline cursor-pointer">
+                            {member.profile.name}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+    )
 }
