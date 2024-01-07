52cd02893b5bb0520bda57ef17cd8b3a03af1677
Prakarti Goel <prakartigoel24@gmail.com>
2024-01-07 15:23:34 +0530

    Add: Constants for help

--- components/chat/chat-item.jsx
+++ components/chat/chat-item.jsx
@@ -3,13 +3,13 @@
 import { Member, MemberRole, Profile } from "@prisma/client";
 import { UserAvatar } from "@/components/user-avatar";
 import { ActionTooltip } from "@/components/action-tooltip";
-import {ShieldAlert, ShieldCheck } from "lucide-react";
+import { ShieldAlert, ShieldCheck } from "lucide-react";
 
 const roleIconMap = {
-    "GUEST": null,
-    "MODERATOR": <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500" />,
-    "ADMIN": <ShieldAlert className="h-4 w-4 ml-2 text-rose-500" />,
-  }
+  GUEST: null,
+  MODERATOR: <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500" />,
+  ADMIN: <ShieldAlert className="h-4 w-4 ml-2 text-rose-500" />,
+};
 
 export const ChatItem = ({
   id,
@@ -21,31 +21,42 @@ export const ChatItem = ({
   currentMember,
   isUpdated,
   socketUrl,
-  socketQuery
-}) => { 
-    return (
-        <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
-        <div className="group flex gap-x-2 items-start w-full">
-            <div className="cursor-pointer hover:drop-shadow-md transition">
-                <UserAvatar src={member.profile.imageUrl}/>
-            </div>
-            <div className="flex flex-col w-full">
-                <div className="flex items-center gap-x-2">
-                    <div className="flex items-center">
-                        <p className="font-semibold text-sm hover:underline cursor-pointer">
-                            {member.profile.name}
-                        </p>
-                        <ActionTooltip label={member.role}>
-                            {roleIconMap[member.role]}
-                        </ActionTooltip>
-                    </div>
-                    <span className="text-xs text-zinc-500 dark:text-zinc-400">
-                        {timestamp}
-                    </span>
-                </div>
-                {content}
-            </div>
+  socketQuery,
+}) => {
+
+  const fileType = fileUrl?.split(".").pop();
+
+  const isAdmin = currentMember.role === MemberRole.ADMIN;
+  const isModerator = currentMember.role === MemberRole.MODERATOR;
+  const isOwner = currentMember.id === member.id;
+  const canDeleteMessage = !deleted && (isAdmin || isModerator || isOwner);
+  const canEditMessage = !deleted && isOwner && !fileUrl;
+  const isPDF = fileType === "pdf" && fileUrl;
+  const isImage = !isPDF && fileUrl;
+
+  return (
+    <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
+      <div className="group flex gap-x-2 items-start w-full">
+        <div className="cursor-pointer hover:drop-shadow-md transition">
+          <UserAvatar src={member.profile.imageUrl} />
         </div>
+        <div className="flex flex-col w-full">
+          <div className="flex items-center gap-x-2">
+            <div className="flex items-center">
+              <p className="font-semibold text-sm hover:underline cursor-pointer">
+                {member.profile.name}
+              </p>
+              <ActionTooltip label={member.role}>
+                {roleIconMap[member.role]}
+              </ActionTooltip>
+            </div>
+            <span className="text-xs text-zinc-500 dark:text-zinc-400">
+              {timestamp}
+            </span>
+          </div>
+          {content}
+        </div>
+      </div>
     </div>
-    )
-}
+  );
+};
