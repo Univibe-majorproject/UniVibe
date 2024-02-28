bca201e566c6783575197660eb1f9b93ef6ef4b8
Prakarti Goel <prakartigoel24@gmail.com>
2024-02-29 00:21:45 +0530

    Added createPost modal type in use-modal-store

--- hooks/use-modal-store.js
+++ hooks/use-modal-store.js
@@ -2,7 +2,7 @@
 import { create } from "zustand";
 
 export const ModalType = "createServer" | "invite" | "editServer" | "members" 
-| "createChannel" | "leaveServer" | "deleteServer" | "deleteChannel" | "editChannel" | "messageFile" | "deleteMessage";
+| "createChannel" | "leaveServer" | "deleteServer" | "deleteChannel" | "editChannel" | "messageFile" | "deleteMessage" | "createPost";
 
 export const useModal = create((set) => ({
   type: null,
