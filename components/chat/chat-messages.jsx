89177330d7f54cf7bf0248ef35fda58b4b458aa2
Prakarti Goel <prakartigoel24@gmail.com>
2024-03-17 22:10:57 +0530

    Fixed error on messages loading late

--- components/chat/chat-messages.jsx
+++ components/chat/chat-messages.jsx
@@ -98,7 +98,7 @@ export const ChatMessages = ({
       <div className="flex flex-col-reverse mt-auto">
         {data?.pages?.map((group, i) => (
           <Fragment key={i}>
-            {group.items.map((message) => (
+            {group?.items?.map((message) => (
               <ChatItem
                 key={message.id}
                 id={message.id}
