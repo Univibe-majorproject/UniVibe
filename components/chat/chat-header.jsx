499c897f8b343cdd32fcb7efbecc10f6a490f779
Prakarti Goel <prakartigoel24@gmail.com>
2024-03-28 00:53:55 +0530

    Test: More profile details form options

--- components/chat/chat-header.jsx
+++ components/chat/chat-header.jsx
@@ -1,12 +1,43 @@
-import { MessageSquareText, Pen } from "lucide-react";
+"use client";
+import { MessageSquareText, Pen , ListPlus} from "lucide-react";
 import { MobileToggle } from "@/components/mobile-toggle";
 import { UserAvatar } from "@/components/user-avatar";
 import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 import { SocketIndicator } from "@/components/socket-indicator";
 import { ChatVideoButton } from "@/components/chat/chat-video-button";
+import {useUser} from "@clerk/nextjs";
+import { useState } from "react";
+
+const MoreDetailsForm = ()=>{
+
+  const { user } = useUser();
+  const [heading, setHeading] = useState("");
+
+  const handleClick = (e)=>{
+    e.preventDefault();
+    user.update({
+      unsafeMetadata: {
+        heading
+      }
+    })
+  }
+  return (
+    <div className="bg-white text-black">
+      <label for="heading">Heading</label>
+      <input type="text" name="heading" id="heading" value={heading} 
+      onChange={(event)=> setHeading(event.target.value)} 
+      className="bg-white border-gray-700 border-4"/>
+      <button type="button" onClick={handleClick}>Update Heading</button>
+
+      <h2>{heading}</h2>
+      <h4>Unsafe Metadata : {user?.unsafeMetadata?.heading}</h4>
+    </div>
+  )
+}
 
 export const ChatHeader = ({ serverId, name, type, imageUrl }) => {
+
   return (
     <div className="text-md font-semibold px-4 flex items-center  border-x-neutral-200 dark:border-[#CBFF01] border-b-2 justify-between h-[80px] dark:bg-[rgb(21,20,29)]">
     <div className="flex items-center justify-center">
@@ -44,7 +75,15 @@ export const ChatHeader = ({ serverId, name, type, imageUrl }) => {
             avatarBox: "h-[48px] w-[48px]",
           },
         }}
-      />
+      >
+        <UserButton.UserProfilePage
+        label="More Details"
+        labelIcon={<ListPlus className="w-5 h-5" />}
+        url="terms"
+      >
+        <MoreDetailsForm/>
+      </UserButton.UserProfilePage>
+      </UserButton>
     </div>
     </div>
   );
